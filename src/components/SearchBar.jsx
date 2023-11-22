4e3f9225ee7366ecdf087f5dee7f399bb7ad194d
Vedant Otari <vedantmotari24@gmail.com>
2023-11-22 16:03:02 +0530

    Added  some files.

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,32 @@
+import { Paper, IconButton } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Search } from "@mui/icons-material";
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component={"form"}
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px soldi red",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="'search-bar"
+        placeholder="Search here..."
+        value={""}
+        onChange={() => {}}
+      />
+      <IconButton type="submit">
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
