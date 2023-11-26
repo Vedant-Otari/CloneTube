aaa7333696ccb59aa1449c3c731f530bfbd84427
Vedant Otari <vedantmotari24@gmail.com>
2023-11-26 17:35:59 +0530

    Video searching feature added.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,22 @@
 import { Paper, IconButton } from "@mui/material";
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
+  const [searchTerm,setSearchTerm] = useState('');
+  const navi = useNavigate();
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(searchTerm){
+      navi(`/search/${searchTerm}`);
+      setSearchTerm('')
+    }
+  }
   return (
     <Paper
       component={"form"}
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px soldi red",
@@ -19,8 +28,8 @@ const SearchBar = () => {
       <input
         className="'search-bar"
         placeholder="Search here..."
-        value={""}
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton type="submit">
         <Search />
