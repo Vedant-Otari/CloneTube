08433500ba01c37d4a69bce37b1b310ce45d3cac
Vedant Otari <vedantmotari24@gmail.com>
2023-11-26 16:17:50 +0530

    Channel Detail page added.

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,12 +1,13 @@
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+import ChannelCard from "./ChannelCard";
+import Videos from "./Videos";
 
 const ChannelDetail = () => {
   const [ChannelDetail, setChannelDetail] = useState(null);
-  const [Videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]);
   const { id } = useParams();
   useEffect(() => {
     fetchFromAPI(`channels?part="snippet&id=${id}`).then((data) => {
@@ -19,7 +20,24 @@ const ChannelDetail = () => {
       }
     );
   }, [id]);
-  return <Box minHeight={"95vh"}></Box>;
+  return (
+    <Box minHeight={"95vh"}>
+      <Box>
+        <div style={{
+          background: 'linear-gradient(337deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)',
+          zIndex:10,
+          height:'200px',
+        }}/>
+      <ChannelCard channelDetail={ChannelDetail} marginTop='-130px'/>
+      </Box>
+      <Box display={'flex'} p='2'>
+        <Box sx={{
+          mr:{sm:'100px'}
+        }}/>
+          <Videos videos={videos}/>
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
